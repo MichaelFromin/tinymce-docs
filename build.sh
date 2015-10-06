e67583c5da6c02c4256137ddc977c3876833ed23
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2015-10-06 14:48:07 -0700

    init

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# exit on firs error
+set -e
+
+# vars
+DESTINATION="_site"
+JEKYLL_ENV="production"
+REFS_PATH="../refs"
+
+# ensure dir existence
+mkdir -p $REFS_PATH
+
+# loop through remote branches
+# and build all versions with changes
+for branch in `git branch -r | grep origin/v`; do
+  echo ""
+  echo " > checking out $branch"
+
+  branch=${branch##*/}
+  git checkout $branch
+
+  current_head=$(git rev-parse HEAD)
+  echo " > current HEAD at $current_head"
+
+  last_head=$(cat $REFS_PATH/$branch 2>/dev/null || true)
+  echo " > last build HEAD at $last_head"
+
+  if [[ $current_head != $last_head ]]; then
+    echo " > $branch has changes, building..."
+    bundle install --no-cache --clean --deployment
+    rm -rf $DESTINATION/$branch
+    jekyll build --destination $DESTINATION/$branch
+    echo $current_head > "$REFS_PATH/$branch"
+  else
+    echo " > $branch is up to date, skipping build"
+  fi
+
+done
+
+echo ""
+echo "done."
